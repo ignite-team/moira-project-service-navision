81eb2a6f5b555f0c1139748bdaed81a9a74c91a7
Dario Aldrey Muiño <a19darioam@iessanclemente.net>
2021-05-24 12:52:53 +0200

    sol

--- src/test/java/es/ozona/moira/project/service/customer/CustomerControllerTest.java
+++ src/test/java/es/ozona/moira/project/service/customer/CustomerControllerTest.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
